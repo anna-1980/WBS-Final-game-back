5f174fadc65139ad2c5eddae3b89ef3eebecb7c6
Anna Zdyb <anna.zdyb@gmail.com>
2022-01-13 16:58:11 +0100

    first commit

---
+++ utils/db/client.js
@@ -0,0 +1,12 @@
+import { MongoClient, MongoCLient } from 'mongodb';
+
+const url = prosecc.evn.MONGO_URI;
+const client = new MongoClient(url);
+
+//DB name
+const dbName = 'games';
+
+const mongoCOnnect = async () => {
+
+}
+
